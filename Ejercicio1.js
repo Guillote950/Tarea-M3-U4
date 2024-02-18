e4d5ffab2548e6cc5ff30d28282a94d80b89fc72
Guillermo Medina <guillermoandresmedina@gmail.com>
2024-02-18 19:35:27 -0300

    Subiendo Ejercicios javascript parte 2

---
+++ Ejercicio1.js
@@ -0,0 +1,14 @@
+// Ejercicio 1
+function cambiarColorRojo() {
+    let rojo = document.querySelector(".rojo");
+    rojo.classList.toggle("clicked");
+  }
+  function cambiarColorAzul() {
+    let azul = document.querySelector(".azul");
+    azul.classList.toggle("clicked");
+  }
+  function cambiarColorVerde() {
+    let verde = document.querySelector(".verde");
+    verde.classList.toggle("clicked");
+  }
+  
---
+++ Ejercicio2.js
@@ -0,0 +1,6 @@
+//Ejercicio 2
+function contarCaracteres() {
+    let textarea = document.getElementById('textArea');
+    let contador = document.getElementById('contador');
+    contador.innerText = textarea.value.length
+}
